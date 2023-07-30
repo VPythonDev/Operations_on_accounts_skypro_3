6979a6945be83bdf3947f53fa231c3974cf7c60f
Valera Babkin <valera.babkin.13@mail.ru>
2023-07-30 19:57:48 +0300

    Changed src/get_operation_data.py Added pytest in poetry

--- src/get_operations_data.py
+++ src/get_operations_data.py
@@ -1,9 +1,7 @@
 import json
 
-path = "data/operations.json"
 
-
-def get_operations_data():
+def get_operations_data(path):
     """Возвращает данные операций из файла operations.json"""
     with open(path, 'r') as operations_data_file:
         operations_data = json.load(operations_data_file)
