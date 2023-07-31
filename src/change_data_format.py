5fab80fbbb0aefb3ca14526189c0afbf9c59f965
Valera Babkin <valera.babkin.13@mail.ru>
2023-07-31 21:35:41 +0300

    Changed change_date Created hide_sender_number

--- src/change_data_format.py
+++ src/change_data_format.py
@@ -1,4 +1,11 @@
 def change_date(operation_data):
     """Изменяет формат даты на ДД.ММ.ГГГГ"""
-    operation_data['date'] = operation_data['date'].strftime("%d.%m.%Y")
-    return operation_data
+    changed_date = operation_data['date'].strftime("%d.%m.%Y")
+    return changed_date
+
+
+def hide_sender_number(operation_data):
+    """Скрывает номер карты отправителя"""
+    sender_number = operation_data['from'].split()[-1]
+    hidden_sender_number = sender_number.replace(sender_number[6:-4], '*' * len(sender_number[6:-4]))
+    return hidden_sender_number
--- tests/test_formatting_data.py
+++ tests/test_formatting_data.py
@@ -4,9 +4,9 @@ import pytest
 
 
 @pytest.mark.parametrize('test_date_list, expected', [
-    ({'date': datetime(2020, 10, 3, 5)}, {'date': '03.10.2020'}),
-    ({'date': datetime(2012, 3, 5)}, {'date': '05.03.2012'}),
-    ({'date': datetime(2022, 12, 12, 12, 12, 12, 12)}, {'date': '12.12.2022'})
+    ({'date': datetime(2020, 10, 3, 5)}, '03.10.2020'),
+    ({'date': datetime(2012, 3, 5)}, '05.03.2012'),
+    ({'date': datetime(2022, 12, 12, 12, 12, 12, 12)}, '12.12.2022')
 ])
 def test_change_date(test_date_list, expected):
     assert change_date(test_date_list) == expected
