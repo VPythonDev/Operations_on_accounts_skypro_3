51119c07533749489edd3fb1db05f7cd721ce583
Valera Babkin <valera.babkin.13@mail.ru>
2023-08-01 00:10:25 +0300

    Changed display_transactions.py Test for show_transactions

--- src/display_transactions.py
+++ src/display_transactions.py
@@ -3,17 +3,17 @@ from src.change_data_format import change_date, hide_sender_number, hide_recipie
 
 def show_transactions(transaction_dict):
     """Подготавливает и выводит данные транзакций"""
-    date = change_date(transaction_dict['date'])
+    date = change_date(transaction_dict)
     description = transaction_dict['description']
 
     if 'from' in transaction_dict:
-        sender_number = transaction_dict['form']
-        hidden_sender_number = sender_number.replace(sender_number.split()[-1], hide_sender_number(sender_number))
+        sender_number = transaction_dict['from']
+        hidden_sender_number = sender_number.replace(sender_number.split()[-1], hide_sender_number(transaction_dict))
     else:
         hidden_sender_number = ''
 
     recipient_number = transaction_dict['to']
-    hidden_recipient_number = recipient_number.replace(recipient_number.split()[-1], hide_recipient_number(recipient_number))
+    hidden_recipient_number = recipient_number.replace(recipient_number.split()[-1], hide_recipient_number(transaction_dict))
 
     amount = transaction_dict['operationAmount']['amount']
     currency = transaction_dict['operationAmount']['currency']['name']
