2256e06b2cbc9003c4204c1bcae57a744bb7189a
Valera Babkin <valera.babkin.13@mail.ru>
2023-07-31 20:38:11 +0300

    Created function data_sorting in src/sorting.py and test

---
+++ tests/test_sorting.py
@@ -0,0 +1,19 @@
+from src.sorting import data_sorting
+from datetime import datetime
+
+
+def test_data_sorting():
+    test_date_list = [
+        {"date": "2018-11-08T22:47:21.935582"},
+        {"date": "2019-12-04T22:47:21.935582"},
+        {"date": "2018-12-06T22:50:21.935582"},
+        {"date": "2019-12-04T22:46:21.935582"}
+    ]
+    expected = [
+        {"date": datetime(2019, 12, 4, 22, 47, 21, 935582)},
+        {"date": datetime(2019, 12, 4, 22, 46, 21, 935582)},
+        {"date": datetime(2018, 12, 6, 22, 50, 21, 935582)},
+        {"date": datetime(2018, 11, 8, 22, 47, 21, 935582)}
+    ]
+
+    assert data_sorting(test_date_list) == expected
